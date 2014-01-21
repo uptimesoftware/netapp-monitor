5bd3f4fc14e579f25ad0eafd99deb054d1112590
Balaji Natarajan <balajin.nsfz@gmail.com>
2014-01-21 17:35:56 +0530

    Set MIBDIRS env-var in files-posix directory script

--- pkg/uptime 7.2/files-posix/scripts/MonitorNetapp/AIX_LPAR_Workload_Monitor.sh
+++ pkg/uptime 7.2/files-posix/scripts/MonitorNetapp/AIX_LPAR_Workload_Monitor.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
-/usr/local/uptime/apache/bin/php MonitorNetapp.php
 
+inst=`grep ^inst /etc/init.d/uptime_httpd | cut -d= -f2`
+MIBDIRS=$inst/mibs
+export MIBDIRS
+
+/usr/local/uptime/apache/bin/php MonitorNetapp.php

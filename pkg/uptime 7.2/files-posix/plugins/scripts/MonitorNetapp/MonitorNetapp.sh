0cf4af6278020394f669665742863f35809aabee
dan <daniel.branton@uptimesoftware.com>
2014-03-13 11:30:47 -0400

    moved the .sh & .bat files into the new folder structure

--- pkg/uptime 7.2/files-posix/plugins/scripts/MonitorNetapp/MonitorNetapp.sh
+++ pkg/uptime 7.2/files-posix/plugins/scripts/MonitorNetapp/MonitorNetapp.sh
@@ -4,4 +4,4 @@ inst=`grep ^inst /etc/init.d/uptime_httpd | cut -d= -f2`
 MIBDIRS=$inst/mibs
 export MIBDIRS
 
-/usr/local/uptime/apache/bin/php ../../plugins/scripts/MonitorNetapp/MonitorNetapp.php
+/usr/local/uptime/apache/bin/php MonitorNetapp.php
--- pkg/uptime 7.2/files-win/scripts/MonitorNetapp/MonitorNetapp.bat
+++ pkg/uptime 7.2/files-win/scripts/MonitorNetapp/MonitorNetapp.bat
@@ -1,3 +1,3 @@
 @ECHO OFF
 set PHPDIR=..\..\apache\php\
-"%PHPDIR%\php.exe" MonitorNetapp.php
+"%PHPDIR%\php.exe" ..\..\plugins\scripts\MonitorNetapp\MonitorNetapp.php
